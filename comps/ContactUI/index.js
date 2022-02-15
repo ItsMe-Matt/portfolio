d983333524c11e22703455f8b2acd6f269db993a
ItsMe_Matt <mzhao42@my.bcit.ca>
2022-02-14 18:24:46 -0800

    styling changes to contact form

--- comps/ContactUI/index.js
+++ comps/ContactUI/index.js
@@ -1,5 +1,5 @@
 
-
+import styled from "styled-components"
 
 
 export default function ContactUI({
@@ -8,17 +8,92 @@ export default function ContactUI({
     message="",
     onSubmitPress=()=>{}
 }) {
-    return <div>
-        <form onSubmit={()=>console.log("hey there bud")}>
-            <p>Name:</p>
-            <input type="text" id="name"></input>
-            <p>Email:</p>
-            <input type="text" id="name"></input>
-            <p>Message:</p>
-            <input type="text" id="name"></input>
+    return <Container>
+        <Left>
+            <LHead>
+                Work with me!
+            </LHead>
 
-            <input type="submit"></input>
-        </form>
+        </Left>
 
-    </div>
+        <Right>
+            <form onSubmit={()=>{console.log("hey there bud");event.preventDefault()}}>
+               <Row>
+               <Cont>
+                   <Label>Name:</Label>
+                   <PInput type="text" id="name"></PInput>
+               </Cont>
+
+               <Cont>
+                   <Label>Email:</Label>
+                   <PInput type="text" id="email"></PInput>
+               </Cont>
+               </Row>
+               
+
+              <Cont>
+                   <Label>Message:</Label>
+                   <MInput type="text" id="message"></MInput>
+              </Cont>
+
+               <input type="submit"></input>
+            </form>
+
+        </Right>
+    </Container>
 }
+
+
+const Container = styled.div`
+display: flex;
+
+width: 75vw;
+
+`
+const Left = styled.div`
+width: 50%
+`
+
+const Right = styled.div`
+width: 50%;
+background-color: red;
+`
+const LHead = styled.h1`
+font-family: Poppins;
+font-weight: 300;
+font-size: 3vw;
+`
+
+const Label = styled.p`
+font-family: Poppins
+`
+
+const Row = styled.div`
+display: flex;
+`
+
+const Cont = styled.div`
+margin: 1vh;
+`
+
+const PInput = styled.input`
+width: 24%;
+padding: 1vh 1vw;
+outline: none;
+
+background-color: transparent;
+
+border: none;
+border-bottom: 0.2vh #737373 solid;
+`
+
+const MInput = styled.input`
+width: 100%;
+padding: 1vh 1vw;
+outline: none;
+
+background-color: transparent;
+
+border: none;
+border-bottom: 0.2vh #737373 solid;
+`
