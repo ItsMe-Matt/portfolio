f98f3a4506dc1d73254eecb5da00a1dfdbdffb0b
ItsMe_Matt <mzhao42@my.bcit.ca>
2022-02-15 22:52:35 -0800

    contact styling changes

--- comps/Social/index.js
+++ comps/Social/index.js
@@ -3,38 +3,19 @@ import { MdOutlineEmail } from "react-icons/md"
 import styled from "styled-components"
 
 
-export default function Social({
-    type="1"
-}){
-    if (type === "1") {
-        return <Container>
-            <IconA href="https://www.linkedin.com/in/mlzhao/" target="_blank">
-                <FaLinkedin size="6vh" color='#2867B2'/>
-            </IconA>
-
-            <IconA href="https://github.com/ItsMe-Matt" target="_blank">
-                <FaGithubSquare size="6vh"/>
-            </IconA>
-
-            <IconC>
-                <MdOutlineEmail size="6vh" />
-            </IconC>
-        </Container>
-    } else if (type === "2") {
-        return <ContainerW>
-            <IconAW href="https://www.linkedin.com/in/mlzhao/" target="_blank">
-                <FaLinkedin size="2vw" color='#2867B2'/>
-            </IconAW>
-
-            <IconAW href="https://github.com/ItsMe-Matt" target="_blank">
-                <FaGithubSquare size="2vw"/>
-            </IconAW>
-
-            <IconC>
-                <MdOutlineEmail size="2vw" />
-            </IconC>
-        </ContainerW>
-    }
+export default function Social(){
+    return <Container>
+        <IconA href="https://www.linkedin.com/in/mlzhao/" target="_blank">
+            <FaLinkedin size="5vh" color='#2867B2'/>
+        </IconA>
+        <IconA href="https://github.com/ItsMe-Matt" target="_blank">
+            <FaGithubSquare size="5vh"/>
+        </IconA>
+        <IconC href="mailto:Matthewlukez@yahoo.com">
+            <MdOutlineEmail size="5vh" />
+        </IconC>
+    </Container>
+    
     
 }
 
@@ -45,42 +26,27 @@ const Container = styled.div`
 `
 
 const IconA = styled.a`
-    padding: 0.35vh;
-    box-shadow: 0 -0.3vh #122c5c inset;
+    padding: 0.5vh 4vh;
+    border: 0.2vh solid #000;
     margin-right: 2vh;
-
-&:hover {
-    transition: 0.25s;
-    border-radius: 1vh;
-    border: 0.3vh solid #2868de;
-}
-`
-const IconC = styled.div`
-    padding: 0.35vh;
-    box-shadow: 0 -0.3vh #122c5c inset;
-
-    &:hover {
-        transition: 0.25s;
-        border-radius: 1vh;
-        border: 0.3vh solid #2868de;
-    }
-`
-const IconAW = styled.a`
-padding: 0.35vh;
-box-shadow: 0 -0.3vh #122c5c inset;
-margin-right: 0.75vw;
-
-&:hover {
-    transition: 0.25s;
-    border-radius: 1vh;
-    border: 0.3vh solid #2868de;
-}
-`
-
-const ContainerW = styled.div`
-    display: flex;
-    width: fit-content;
-    flex-direction: row;
-
+    border-radius: 5rem;
     
+
+&:hover {
+    transition: 0.5s;
+    border: 0.3vh solid #2868de;
+    box-shadow: 0 0.4vh 0.2vh #505050;
+}
+`
+const IconC = styled.a`
+padding: 0.5vh 4vh;
+border: 0.2vh solid #000;
+margin-right: 2vh;
+border-radius: 5rem;
+
+&:hover {
+transition: 0.5s;
+border: 0.3vh solid #2868de;
+box-shadow: 0 0.4vh 0.2vh #505050;
+}
 `
