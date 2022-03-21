98b6e71cbefc8e9a8f9c136f1292cc728b5ad9c0
ItsMe-Matt <mzhao42@my.bcit.ca>
2022-03-20 23:36:15 -0700

    fixing for vercel

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,6 @@ import { useState } from 'react';
 import styled from 'styled-components'
 import Link from 'next/link'
 import { BsArrowDownShort } from 'react-icons/bs'
-import Head from 'next/head'
 
 import ContactUI from '../comps/ContactUI';
 import ContentHead from "../comps/ContentHead";
@@ -64,9 +63,6 @@ export default function Home() {
   }
 
   return (<div>
-    <Head>
-      <title>Matt's Portfolio</title>
-    </Head>
     <Container>
     <HeadNav />
       <Hero>
