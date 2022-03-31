861b5faaf294d6e5e68fdd69bef2ce4b89034b87
ItsMe-Matt <mzhao42@my.bcit.ca>
2022-03-31 01:22:16 -0700

    typo fixing

--- comps/HeadNav/index.js
+++ comps/HeadNav/index.js
@@ -64,7 +64,7 @@ export default function HeadNav() {
                     <ProjectBtn
                     onClick={()=>{setShowModal(false)}}
                     >
-                    Project
+                    Projects
                     </ProjectBtn> 
                 </Link>
 
